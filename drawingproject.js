26fb57eeb95acddaf0d8bc144074b1ebe09d5e9a
Miguel Angel Castillo <321angelcastillo.mc@gmail.com>
2016-10-05 11:05:43 -0700

    ...

--- drawingproject.js
+++ drawingproject.js
@@ -2,5 +2,5 @@ var canvasDiv = document.getElementById("drawingArea");
 var canvas = canvasDiv.getContext('2d');
 
 canvas.fillStyle = "pink";
-canvas.fillRect(80, 50, 100, 140);
-// 1st#, left wall; 2nd#, up and down; 3rd#, length;
+canvas.fillRect(80, 50, 100, 100);
+// 1st#, left wall; 2nd#, up and down; 3rd#, length; 4th, width;
