64da7eda111f4c66e4b52649e5dfca51f2ae5782
Miguel Angel Castillo <321angelcastillo.mc@gmail.com>
2016-10-06 11:01:00 -0700

    I have the second pumpkin.

--- drawingproject.js
+++ drawingproject.js
@@ -5,19 +5,24 @@ var canvas = canvasDiv.getContext('2d');
 // 1st#, left wall; 2nd#, up and down; 3rd#, length; 4th, width;
 //rectangle pumpkin #1
 canvas.fillStyle = "brown";
-canvas.fillRect(235, 485, 30, 50);
+canvas.fillRect(195, 485, 30, 50);
 
 canvas.beginPath();
 canvas.fillStyle = "orange";
-canvas.arc(250,600,85,0,2*Math.PI,true);
+canvas.arc(200,600,85,0,2*Math.PI,true);
+canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "orange";
+canvas.arc(160,600,80,0,2*Math.PI,true);
 canvas.fill();
 
 canvas.beginPath();
 canvas.fillStyle = "orange";
 canvas.arc(220,600,80,0,2*Math.PI,true);
 canvas.fill();
-
+//2nd pumpkin
 canvas.beginPath();
 canvas.fillStyle = "orange";
-canvas.arc(280,600,80,0,2*Math.PI,true);
+canvas.arc(450,600,85,0,2*Math.PI,true);
 canvas.fill();
