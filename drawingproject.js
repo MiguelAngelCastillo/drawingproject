b0cfa86fc64f5f56819e9ea6e3f68621c5044413
Miguel Angel Castillo <321angelcastillo.mc@gmail.com>
2016-10-06 10:36:45 -0700

    Im trying to make a pumpkin . . . is that how you spell it?

--- drawingproject.js
+++ drawingproject.js
@@ -1,7 +1,7 @@
 var canvasDiv = document.getElementById("drawingArea");
 var canvas = canvasDiv.getContext('2d');
 
-canvas.fillStyle = "pink";
+//canvas.fillStyle = "pink";
 //canvas.fillRect(80, 50, 100, 100);
 // 1st#, left wall; 2nd#, up and down; 3rd#, length; 4th, width;
 
@@ -18,11 +18,4 @@ canvas.fill();
 canvas.beginPath();
 canvas.fillStyle = "orange";
 canvas.arc(280,600,80,0,2*Math.PI,true);
-canvas.fill();
-
-canvas.beginPath();
-canvas.fillStyle = "black";
-canvas.moveTo(200,122);
-canvas.lineTo(220,126);
-canvas.lineTo(200,130);
 canvas.fill();
