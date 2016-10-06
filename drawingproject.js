0d4fbfbd7e076f476b98fc6ce18c5b1e277009f2
Miguel Angel Castillo <321angelcastillo.mc@gmail.com>
2016-10-06 10:48:06 -0700

    I have a pumpkin!!!

--- drawingproject.js
+++ drawingproject.js
@@ -1,9 +1,11 @@
 var canvasDiv = document.getElementById("drawingArea");
 var canvas = canvasDiv.getContext('2d');
-
 //canvas.fillStyle = "pink";
 //canvas.fillRect(80, 50, 100, 100);
 // 1st#, left wall; 2nd#, up and down; 3rd#, length; 4th, width;
+//rectangle pumpkin #1
+canvas.fillStyle = "brown";
+canvas.fillRect(235, 485, 30, 50);
 
 canvas.beginPath();
 canvas.fillStyle = "orange";
