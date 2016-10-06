bb2ad64fafa4b605dcc3d1d97ed150d1dbcbadda
Miguel Angel Castillo <321angelcastillo.mc@gmail.com>
2016-10-06 11:13:59 -0700

    Im just trying to add some triangles.

--- drawingproject.js
+++ drawingproject.js
@@ -3,6 +3,8 @@ var canvas = canvasDiv.getContext('2d');
 //canvas.fillStyle = "pink";
 //canvas.fillRect(80, 50, 100, 100);
 // 1st#, left wall; 2nd#, up and down; 3rd#, length; 4th, width;
+canvas.fillStyle = "DarkOrange";
+canvas.fillRect(0,0,1000,800);
 //rectangle pumpkin #1
 canvas.fillStyle = "brown";
 canvas.fillRect(175, 485, 30, 50);
@@ -20,6 +22,13 @@ canvas.fill();
 canvas.beginPath();
 canvas.fillStyle = "orange";
 canvas.arc(220,600,80,0,2*Math.PI,true);
+canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "orange";
+canvas.moveTo(200,122);
+canvas.lineTo(220,126);
+canvas.lineTo(220,126);
 canvas.fill();
 //2nd pumpkin
 canvas.fillStyle = "brown";
