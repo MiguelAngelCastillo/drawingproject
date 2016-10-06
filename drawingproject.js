bd58d88502d170489662095ce98bf4364eaae8ee
Miguel Angel Castillo <321angelcastillo.mc@gmail.com>
2016-10-06 11:01:07 -0700

    2nd

--- drawingproject.js
+++ drawingproject.js
@@ -5,11 +5,11 @@ var canvas = canvasDiv.getContext('2d');
 // 1st#, left wall; 2nd#, up and down; 3rd#, length; 4th, width;
 //rectangle pumpkin #1
 canvas.fillStyle = "brown";
-canvas.fillRect(195, 485, 30, 50);
+canvas.fillRect(175, 485, 30, 50);
 
 canvas.beginPath();
 canvas.fillStyle = "orange";
-canvas.arc(200,600,85,0,2*Math.PI,true);
+canvas.arc(190,600,85,0,2*Math.PI,true);
 canvas.fill();
 
 canvas.beginPath();
@@ -22,7 +22,20 @@ canvas.fillStyle = "orange";
 canvas.arc(220,600,80,0,2*Math.PI,true);
 canvas.fill();
 //2nd pumpkin
+canvas.fillStyle = "brown";
+canvas.fillRect(455, 485, 30, 50);
+
 canvas.beginPath();
 canvas.fillStyle = "orange";
-canvas.arc(450,600,85,0,2*Math.PI,true);
+canvas.arc(470,600,85,0,2*Math.PI,true);
+canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "orange";
+canvas.arc(440,600,80,0,2*Math.PI,true);
+canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "orange";
+canvas.arc(500,600,80,0,2*Math.PI,true);
 canvas.fill();
