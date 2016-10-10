2036b6276a75e563c4cf11f8d6c1e3e8ecc8f46e
Miguel Angel Castillo <321angelcastillo.mc@gmail.com>
2016-10-10 11:51:59 -0700

    parts added to explain code.

--- drawingproject.js
+++ drawingproject.js
@@ -6,9 +6,10 @@ var canvas = canvasDiv.getContext('2d');
 canvas.fillStyle = "DarkOrange";
 canvas.fillRect(0,0,1000,800);
 //rectangle pumpkin #1
+//stem
 canvas.fillStyle = "brown";
 canvas.fillRect(175, 485, 30, 50);
-
+//body
 canvas.beginPath();
 canvas.fillStyle = "orange";
 canvas.arc(190,600,85,0,2*Math.PI,true);
@@ -23,17 +24,25 @@ canvas.beginPath();
 canvas.fillStyle = "orange";
 canvas.arc(220,600,80,0,2*Math.PI,true);
 canvas.fill();
-
+//eyes
 canvas.beginPath();
 canvas.fillStyle = "black";
 canvas.moveTo(240,550);
 canvas.lineTo(230,580);
 canvas.lineTo(250,580);
 canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "black";
+canvas.moveTo(140,550);
+canvas.lineTo(130,580);
+canvas.lineTo(150,580);
+canvas.fill();
 //2nd pumpkin
+//stem
 canvas.fillStyle = "brown";
 canvas.fillRect(455, 485, 30, 50);
-
+//body
 canvas.beginPath();
 canvas.fillStyle = "orange";
 canvas.arc(470,600,85,0,2*Math.PI,true);
@@ -48,17 +57,32 @@ canvas.beginPath();
 canvas.fillStyle = "orange";
 canvas.arc(500,600,80,0,2*Math.PI,true);
 canvas.fill();
-
+//hat
 canvas.beginPath();
 canvas.fillStyle = "black";
 canvas.moveTo(480,330);
 canvas.lineTo(340,560);
 canvas.lineTo(600,560);
 canvas.fill();
+//eyes
+canvas.beginPath();
+canvas.fillStyle = "black";
+canvas.moveTo(515,550);
+canvas.lineTo(500,580);
+canvas.lineTo(530,580);
+canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "black";
+canvas.moveTo(425,550);
+canvas.lineTo(440,580);
+canvas.lineTo(410,580);
+canvas.fill();
 //3rd pumpkin
+//stem
 canvas.fillStyle = "brown";
 canvas.fillRect(735, 485, 30, 50);
-
+//body
 canvas.beginPath();
 canvas.fillStyle = "orange";
 canvas.arc(750,600,85,0,2*Math.PI,true);
@@ -72,4 +96,18 @@ canvas.fill();
 canvas.beginPath();
 canvas.fillStyle = "orange";
 canvas.arc(780,600,80,0,2*Math.PI,true);
+canvas.fill();
+//eyes
+canvas.beginPath();
+canvas.fillStyle = "black";
+canvas.moveTo(700,550);
+canvas.lineTo(690,580);
+canvas.lineTo(710,580);
+canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "black";
+canvas.moveTo(700,550);
+canvas.lineTo(690,580);
+canvas.lineTo(710,580);
 canvas.fill();
